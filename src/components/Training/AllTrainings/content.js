b8beebd230d64edefa4ad6f7d27541b2efd4a547
Alexandre Lissac <alex36330@gmail.com>
2019-12-08 17:14:09 +0100

    Add training button push new training to db. Entrance key fetch after push then use to in url to access the edit training page

--- src/components/Training/AllTrainings/content.js
+++ src/components/Training/AllTrainings/content.js
@@ -3,7 +3,10 @@ import WeekTrainingTable from "./WeekTrainingTable";
 import {Button, makeStyles} from "@material-ui/core";
 import AddCircleIcon from '@material-ui/icons/AddCircle';
 import {Link} from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import * as ROUTES from "../../../constants/routes";
+import firebase from "firebase";
+
 
 const useStyles = makeStyles({
     button: {
@@ -15,6 +18,20 @@ const useStyles = makeStyles({
 
 const TrainingContent = () => {
     const classes = useStyles();
+    const dbRef = firebase.database().ref('trainings');
+    let history = useHistory();
+
+    const handleNewTraining = () => {
+        dbRef.push({
+            date: '22/05/2020',
+            overallPerformance: 0,
+            playerAttendees: '',
+        }).then((snap) => {
+            const key = snap.key;
+            history.push('/trainings/' + key)
+        })
+    };
+
     return (
         <div>
             <h1>Entraînement</h1>
@@ -25,7 +42,8 @@ const TrainingContent = () => {
                 color="primary"
                 startIcon={<AddCircleIcon/>}
                 className={classes.button}
-                component={Link} to={ROUTES.INPUTTRAININGS}
+                //
+                onClick={() => handleNewTraining()}
             >
                 Ajouter
             </Button>
