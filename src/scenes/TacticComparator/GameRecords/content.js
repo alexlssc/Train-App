206cfc654106e62008729a281a75aef6c1eb1af3
Alexandre Lissac <alex36330@gmail.com>
2019-12-30 12:32:42 +0100

    Json file added included all ligue2 teams with link to their logo

--- src/scenes/TacticComparator/GameRecords/content.js
+++ src/scenes/TacticComparator/GameRecords/content.js
@@ -3,18 +3,20 @@ import {makeStyles} from "@material-ui/core";
 import TacticComparatorTable from "./components/Table";
 import AddCircleIcon from '@material-ui/icons/AddCircle';
 import {Button} from "@material-ui/core";
+import clubData from "../../../constants/clubs.json";
 
 const useStyle = makeStyles({
     button: {
         marginTop: 20,
         float: 'right',
     }
-})
+});
 
 const GameRecords = () => {
     const classes = useStyle();
     return (
         <React.Fragment>
+            {console.log(clubData)}
             <TacticComparatorTable/>
             <Button
                 variant="contained"
